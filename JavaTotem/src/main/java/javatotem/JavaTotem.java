071f349b8122b4dc5946333c84df5d320135660f
Victor Xavier <vihugox@gmail.com>
2024-05-09 12:14:18 -0300

    adicionamento do projeto
    Projeto ainda em construção

---
+++ JavaTotem/src/main/java/javatotem/JavaTotem.java
@@ -0,0 +1,11 @@
+package javatotem;
+
+import apresentacao.Iniciar;
+
+public class JavaTotem {
+
+    public static void main(String[] args) {
+        Iniciar iniciar = new Iniciar(null, true);
+        iniciar.setVisible(true);
+    }
+}
